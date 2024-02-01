cddac2943426d25a13f23fd5886a22c86aec2a61
engine-gh <engine-gh@proton.me>
2024-02-01 15:58:26 +0530

    Refactor notebook execution code

--- index.ts
+++ index.ts
@@ -10,38 +10,31 @@ const STOP_EXECUTION_XPATH = `//span[text()="Stop execution"]`;
 const URL = process.env.DEEPNOTE_URL!;
 
 async function main() {
-    const browser = await puppeteer
-        .use(StealthPlugin())
-        .launch({
-            headless: "new",
-            userDataDir: "./user_datas",
-        });
-    try {
+    const browser = await puppeteer.use(StealthPlugin()).launch({
+        headless: "new",
+        userDataDir: "./user_data",
+    });
 
-        const page = await browser.newPage();
-        await page.goto(URL);
-        await page.waitForNetworkIdle();
-        await page.waitForXPath(STATUS_INDICATOR_XPATH);
+    const page = await browser.newPage();
+    await page.goto(URL);
+    await page.waitForNetworkIdle();
+    await page.waitForXPath(STATUS_INDICATOR_XPATH);
 
-        const ele = await page.$(RUN_NOTEBOOK_SELECTOR);
-        if (!ele) {
-            const stop_ele = await page.$x(STOP_EXECUTION_XPATH);
-            if (stop_ele.length > 0) {
-                console.log("Notebook is already running");
-            } else {
-                console.log("Can't find run button");
-            }
+    const ele = await page.$(RUN_NOTEBOOK_SELECTOR);
+    if (!ele) {
+        const stop_ele = await page.$x(STOP_EXECUTION_XPATH);
+        if (stop_ele.length > 0) {
+            console.log("Notebook is already running");
         } else {
-            await ele.click();
-            await page.waitForXPath(STOP_EXECUTION_XPATH);
-            console.log("Stated notebook");
+            throw new Error("Notebook is not running and cannot be started");
         }
-
-        await browser.close();
-    } catch (error:any) {
-        console.error("Error", error.message);
-        await browser.close();
+    } else {
+        await ele.click();
+        await page.waitForXPath(STOP_EXECUTION_XPATH);
+        console.log("Stated notebook");
     }
+
+    await browser.close();
 }
 
 main();
